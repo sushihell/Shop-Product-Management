c85f9619bd25a766096dabeed61c8a40e5fcdc59
Monowar Hossain Munna <monowarHossain.personal@gmail.com>
2025-04-17 09:11:12 +0600

    Update CustomerInfo.java

--- src/main/java/order/CustomerInfo.java
+++ src/main/java/order/CustomerInfo.java
@@ -8,19 +8,40 @@ public class CustomerInfo {
     public CustomerInfo(String name,String location,int phoneNumber,String email){
         this.name = name;
         this.location = location;
-        this.phoneNumber = phoneNumber;
         this.email = email;
+        try {
+            if (String.valueOf(phoneNumber).length() != 11) {
+                throw new IllegalArgumentException("Phone number must be 11 digits long.");
+            }
+            this.phoneNumber = phoneNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error with phone number: " + e.getMessage());
+        }
     }
     public CustomerInfo(String name,String location,int phoneNumber){
         this.name = name;
         this.location = location;
-        this.phoneNumber = phoneNumber;
+        try {
+            if (String.valueOf(phoneNumber).length() != 11) {
+                throw new IllegalArgumentException("Phone number must be 11 digits long.");
+            }
+            this.phoneNumber = phoneNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error with phone number: " + e.getMessage());
+        }
     }
     public void UpdateInfo(String name,String location,int phoneNumber,String email){
         this.name = name;
         this.location = location;
-        this.phoneNumber = phoneNumber;
         this.email = email;
+        try {
+            if (String.valueOf(phoneNumber).length() != 11) {
+                throw new IllegalArgumentException("Phone number must be 11 digits long.");
+            }
+            this.phoneNumber = phoneNumber;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error with phone number: " + e.getMessage());
+        }
     }
 
     public int getPhoneNumber() {
